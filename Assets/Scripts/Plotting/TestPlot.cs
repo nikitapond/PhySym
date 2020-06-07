83d0294899d7a4b6b39eae6892a33fb7a6a20ebd
nikitapond <63856590+nikitapond@users.noreply.github.com>
2020-06-07 12:53:55 +0100

    Added Vector field plotting (unf)

---
+++ Assets/Scripts/Plotting/TestPlot.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlot : MonoBehaviour
+{
+
+    private VectorFieldPlotter Plotter;
+
+    struct Charge
+    {
+        public Vector3 Position;
+        public float Magnitude;
+        public Charge(Vector3 pos, float mag)
+        {
+            Position = pos;
+            Magnitude = mag;
+        }
+    }
+
+    private Charge[] Charges;
+    private FieldRange Range;
+    private void Awake()
+    {
+        Plotter = GetComponent<VectorFieldPlotter>();
+
+        Charges = new Charge[] { 
+            new Charge(new Vector3(5, 0, 0), 1),
+            new Charge(new Vector3(0, 0, 0), 1),
+            new Charge(new Vector3(5, 0, 5), 1),
+            new Charge(new Vector3(0, 0, 5), 1),
+        };
+
+        Range = new FieldRange(new Vector3(-1, -1, -1), new Vector3(6, 1, 6));
+
+    }
+
+    void Start()
+    {
+        Plotter.SetFunction(FieldFunction);
+        Plotter.SetRangeAndResolution(Range, 0.5f);
+    }
+    
+
+    public Vector3 FieldFunction(float x, float y, float z)
+    {
+        Vector3 pos = new Vector3(x, y, z);
+        Vector3 value = Vector3.zero;
+
+        foreach(Charge c in Charges)
+        {
+            Vector3 disp = c.Position - pos;
+            value += disp * c.Magnitude / disp.sqrMagnitude;
+
+        }
+
+        return value;
+
+
+    }
+
+
+}
--- Assets/Scripts/Plotting/VectorFieldPlotter.cs
+++ Assets/Scripts/Plotting/VectorFieldPlotter.cs
@@ -7,11 +7,46 @@ public class VectorFieldPlotter : MonoBehaviour
 
     public delegate Vector3 Function(float x, float y, float z);
 
+    private GameObject ArrowPrefab;
+
     private Function FieldFunction;
 
     private FieldRange Range;
+    private float Resolution;
+    private Vec3i DataSize;
+
     private Vector3[,,] Data;
-    private 
+
+    private void Awake()
+    {
+        ArrowPrefab = Resources.Load<GameObject>("Models/Arrow");
+        Resolution = -1;
+    }
+
+    private void Start()
+    {
+        if (FieldFunction == null)
+            throw new ArgumentException("Cannot plot field without field function");
+        if (Resolution < 0)
+            throw new ArgumentException("Must set range and resolution of system");
+        if (Data == null)
+            CalculateFieldValues();
+
+        for (int x = 0; x < DataSize.x; x++)
+        {
+            for (int y = 0; y < DataSize.y; y++)
+            {
+                for (int z = 0; z < DataSize.z; z++)
+                {
+                    GameObject arrow = Instantiate(ArrowPrefab);
+                    arrow.transform.parent = transform;
+                    arrow.transform.localPosition = new Vector3(x, y, z) * Resolution + Range.Minimum;
+                    arrow.transform.rotation = Quaternion.LookRotation(Data[x, y, z]);
+                    arrow.transform.localScale = Vector3.one * Data[x, y, z].magnitude;
+                }
+            }
+        }
+    }
 
     public void SetFunction(Function func)
     {
@@ -24,14 +59,36 @@ public class VectorFieldPlotter : MonoBehaviour
     /// <param name="resolution">The number of points per unit to display</param>
     public void SetRangeAndResolution(FieldRange range, float resolution)
     {
+        if (resolution < 0)
+            throw new ArgumentException("Resolution must be positive");
         Range = range;
+        Resolution = resolution;
+        int xSize = Mathf.CeilToInt(range.Size.x / resolution);
+        int ySize = Mathf.CeilToInt(range.Size.y / resolution);
+        int zSize = Mathf.CeilToInt(range.Size.z / resolution);
+        DataSize = new Vec3i(xSize, ySize, zSize);
     }
     /// <summary>
     /// Calculates the field at each point within <see cref="Range"/>
     /// </summary>
     public void CalculateFieldValues()
     {
-
+        //We initiate our data array
+        Data = new Vector3[DataSize.x, DataSize.y, DataSize.z];
+        for(int x=0; x<DataSize.x; x++)
+        {
+            for(int y=0; y<DataSize.y; y++)
+            {
+                for(int z=0; z<DataSize.z; z++)
+                {
+                    float x_ = Range.Maximum.x + Resolution * x;
+                    float y_ = Range.Maximum.y + Resolution * y;
+                    float z_ = Range.Maximum.z + Resolution * z;
+                    Vector3 val = FieldFunction(x_, y_, z_);
+                    Data[x, y, z] = val;
+                }
+            }
+        }
     }
 
 }
@@ -39,6 +96,9 @@ public struct FieldRange
 {
     public Vector3 Minimum;
     public Vector3 Maximum;
+
+    public Vector3 Size;
+
 
     public FieldRange(Vector3 min, Vector3 max)
     {
@@ -53,5 +113,6 @@ public struct FieldRange
 
         Minimum = new Vector3(minX, minY, minZ);
         Maximum = new Vector3(maxX, maxY, maxZ);
+        Size = Maximum - Minimum;
     }
 }
